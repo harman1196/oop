828bf663909c87b03f82e9ff354f200adf32b7b8
747624 <747624@123-PC10.student.Tor.Loc>
2019-06-11 15:21:13 -0400

    second

---
+++ src/oop/ArrayListExamples.java
@@ -0,0 +1,33 @@
+package oop;
+
+import java.awt.List;
+
+public class ArrayListExamples {
+
+	public static void main(String[] args) {
+		// 1. Create a new List
+		List names = new List();
+		
+		// 1. Add 2 people to the list
+		
+		
+		// 2. Output total number of people in list
+		
+		
+		// 3. Delete someone from the list
+		
+		// 4. Output all people in the list
+		
+		
+		// 5. Get one person out of the list
+		
+		// 6. Change the name of the person
+		
+		// 7. Delete everyone from list
+		
+		// 8. Loop through every item in the list and output:
+		// HELLO _______ (where ___ is the name)
+		
+	}
+
+}
